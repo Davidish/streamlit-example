a9bfb79d658b5793ce1ffe8890b0847e9878df93
David Isherwood <davantish@gmail.com>
2024-05-19 10:04:23 +0000

    Add recipes to select

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,17 @@ recipes = response.data
 
 import streamlit as st
 
+recipe_display = [recipes['name'] for reciple in recipe.values()]
+
+# for recipe in recipes:
+#         # print(recipe)
+#         recipe_id = recipe['recipe_id']
+#         recipe_name = recipe['name']
+#         recipe_display.append(recipe_display)
+
 options = st.multiselect(
     "Choose recipes for the week",
-    [recipes])
+    [recipe_display])
 
 st.write("You selected:", options)
 
