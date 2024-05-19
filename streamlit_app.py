0144527c2813de325860338801eaa87c288b0f61
David Isherwood <davantish@gmail.com>
2024-05-19 09:48:53 +0000

    Update to meal planner

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,29 @@
-import datetime
 import streamlit as st
-import pandas as pd
-from os.path import exists
-import pathlib
 
-file_exists = exists(pathlib.Path('data.parquet'))
-st.title("Harry's Timesheet App")
+st.title("Mielie Meal Planner")
 
-t1 = st.time_input('Start time:', datetime.time(8, 00))
-t2 = st.time_input('End time:', datetime.time(8, 00))
-user = 'system'
-insert_datetime = datetime.datetime.now()
+st.write('Test')
 
-if st.button('Submit'):
-    if file_exists:
-        df = pd.read_parquet('data.parquet')
-        details = {
-            'start_time' : [t1],
-            'end_time' : [t2],
-            'user' : [user],
-            'insert_datetime':[insert_datetime]
-        }
-        df_insert = pd.DataFrame(details)
-        df = pd.concat([df, df_insert], ignore_index=True, sort=False)        
-    else:
-        details = {
-            'start_time' : [t1],
-            'end_time' : [t2],
-            'user' : [user],
-            'insert_datetime':[insert_datetime]
-        }
-        df = pd.DataFrame(details)
-        
-    df.to_parquet('data.parquet')
-    st.write('Entry submitted...')
-    st.dataframe(df)
+url = os.environ["SUPABASE_URL"]
+key = os.environ["SUPABASE_KEY"]
+
+supabase: Client = create_client(url, key)
+
+response = supabase.table('recipe').select("*").execute()
+recipes = response.data
+
+for recipe in recipes:
+        # print(recipe)
+        recipe_id = recipe['recipe_id']
+        recipe_name = recipe['name']
+        # print(f'Recipe id: {recipe_id}')
+        # print(f'Recipe name: {recipe_name}')
+
+        st.write(f'Recipe name: {recipe_name}')
+
+# options = st.multiselect(
+#     "What are your favorite colors",
+#     ["Green", "Yellow", "Red", "Blue"],
+#     ["Yellow", "Red"])
+
+# st.write("You selected:", options)
