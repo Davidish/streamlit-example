d15f60265da0b3eb22c86b96eb3299ebaa02f70d
David Isherwood <davantish@gmail.com>
2024-05-19 10:26:44 +0000

    Find chosen recipe ids

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,14 @@ import os
 from supabase import create_client, Client
 
 st.title("Mielie Meal Planner")
-
-st.write('Choose your meals!')
+st.write('Welcome!')
 
 url = os.environ["SUPABASE_URL"]
 key = os.environ["SUPABASE_KEY"]
-
-st.write(url)
-
 supabase: Client = create_client(url, key)
 
 response = supabase.table('recipe').select("*").execute()
 recipes = response.data
-
-import streamlit as st
 
 recipe_display = [recipe['name'] for recipe in recipes]
 
@@ -27,24 +21,36 @@ recipe_display = [recipe['name'] for recipe in recipes]
 #         recipe_display.append(recipe_display)
 
 options = st.multiselect(
-    "Choose recipes for the week",
+    "Choose your recipes:",
     recipe_display)
 
 st.write("You selected:", options)
 
+matching_recipe_ids = [recipe['id'] for recipe in recipes if recipe['name'] in options]
 
-# for recipe in recipes:
-#         # print(recipe)
-#         recipe_id = recipe['recipe_id']
-#         recipe_name = recipe['name']
-#         # print(f'Recipe id: {recipe_id}')
-#         # print(f'Recipe name: {recipe_name}')
+st.write(matching_recipe_ids)
 
-#         st.write(f'Recipe name: {recipe_name}')
+for recipe in recipes:
+        # print(recipe)
+        recipe_id = recipe['recipe_id']
+        recipe_name = recipe['name']
+        # print(f'Recipe id: {recipe_id}')
+        print(f'Recipe name: {recipe_name}')
 
-# options = st.multiselect(
-#     "What are your favorite colors",
-#     ["Green", "Yellow", "Red", "Blue"],
-#     ["Yellow", "Red"])
+        reponse_ingredients = supabase.table('recipeingredient').select("*").eq('recipe_id', recipe_id).execute()
 
-# st.write("You selected:", options)
+        ingredient_ids = reponse_ingredients.data
+        for ingredient_id in ingredient_ids:
+            id = ingredient_id['ingredient_id']
+            quantity = ingredient_id['quantity']
+
+            reponse_final = supabase.table('ingredient').select("*").eq('ingredient_id',id).execute()
+            final = reponse_final.data[0]
+
+            ingredient_name = final['name']
+            ingredient_type = final['type']
+            ingredient_category = final['category']
+            ingredient_unit = final['unit']
+
+            
+            print(f'{ingredient_category}: {ingredient_name} {ingredient_type} = {quantity} {ingredient_unit}')
